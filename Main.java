c5177e0f29019397e190dbf0a10f9cfa7b211cab
amcs1624-dot <amcs1624@gmail.com>
2026-04-07 14:13:04 -0500

    Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,38 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sistema hospital iniciado");
+
+        List<Examen> examenes = new ArrayList<>();
+        List<Tratamiento> tratamientos = new ArrayList<>();
+        List<String> efectos = new ArrayList<>();
+        List<Paciente> pacientes = new ArrayList<>();
+
+        efectos.add("Náuseas");
+
+        Tratamiento t1 = new Tratamiento("T01", "Quimioterapia", 30, efectos);
+        Examen e1 = new Examen("E01", LocalDate.now(), "Biopsia", "Pendiente");
+
+        examenes.add(e1);
+        tratamientos.add(t1);
+
+        Paciente p1 = new Paciente(
+                "12345",
+                "Ana López",
+                45,
+                "Femenino",
+                "Leucemia",
+                "II",
+                "En tratamiento",
+                examenes,
+                tratamientos
+        );
+
+        Sistema sistema = new Sistema(pacientes);
+        sistema.agregarPaciente(p1);
+
+        System.out.println("Sistema funcionando correctamente 🚀");
     }
 }
